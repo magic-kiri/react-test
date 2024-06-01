9cb261f71f4bd032412026c22392e6a954a79899
magic-kiri <kimuapon@gmail.com>
2024-06-01 14:00:55 +0600

    init task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rowStyle = {
+  display: "flex",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const squareStyle = {
+  width: "60px",
+  height: "60px",
+  backgroundColor: "#ddd",
+  margin: "4px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "20px",
+  color: "white",
+};
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+};
+
+const instructionsStyle = {
+  marginTop: "5px",
+  marginBottom: "5px",
+  fontWeight: "bold",
+  fontSize: "16px",
+};
+
+const buttonStyle = {
+  marginTop: "15px",
+  marginBottom: "16px",
+  width: "80px",
+  height: "40px",
+  backgroundColor: "#8acaca",
+  color: "white",
+  fontSize: "16px",
+};
+
+function Square() {
+  return <div className="square" style={squareStyle}></div>;
+}
+
+function Board() {
+  return (
+    <div style={containerStyle} className="gameBoard">
+      <div id="statusArea" className="status" style={instructionsStyle}>
+        Next player: <span>X</span>
+      </div>
+      <div id="winnerArea" className="winner" style={instructionsStyle}>
+        Winner: <span>None</span>
+      </div>
+      <button style={buttonStyle}>Reset</button>
+      <div className="board-row" style={rowStyle}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row" style={rowStyle}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row" style={rowStyle}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Game />);
